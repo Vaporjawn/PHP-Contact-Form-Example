7de6c7dc9e7d742c07bbcada53b58b8b3dc6fb6d
Victor Williams <vrwilliams719@gmail.com>
2020-01-21 07:15:18 -0500

    Edits

--- index.php
+++ index.php
@@ -3,10 +3,19 @@
 <head>
 	<meta charset='utf-8'>
 	<title> Contact Form Example</title>
+	<link href='https://fonts.gooleapis.com/css?family=Roboto+Condensed:400, 700' rel ='stylesheet' href='style.css'>
+	<link rel='stylesheet' href='style.css'>
 </head>
 <body>
 	<main>
 		<p> SEND E-MAIL </p>
+		<form class="contact-form" action='contactform.php' meathod='post'>
+			<input type='text' name='name' placeholder='Full name'>	
+			<input type='text' name='mail' placeholder='Your E-mail'>	
+			<input type='text' name='subject' placeholder='Subject'>	
+			<textarea name='message' placeholder='Message'></textarea>
+			<button type='submit' name='submit'> SEND EMAIL </button>
+				</form>
 	</main>
 </body>
 </html>
